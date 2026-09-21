75a4b220a5b61ea56c2a20f2cec5c8f74fb69db8
0xSunill <sunday7637@gmail.com>
2026-09-21 17:01:00 +0530

    channels

--- learning/day03-channels/src/main.rs
+++ learning/day03-channels/src/main.rs
@@ -3,24 +3,31 @@
 // #[tokio::main]
 // async fn main() {
 //     let (sender, mut receiver) = mpsc::channel(10);
-
 //     let producer = tokio::spawn(async move {
 //         sender.send(100).await.unwrap();
 //         sender.send(200).await.unwrap();
 //         sender.send(300).await.unwrap();
-        
-//     });
 
+//     });
 //     let consumer = tokio::spawn(async move {
 //         while let Some(value) = receiver.recv().await {
 //             println!("Received: {}", value);
 //         }
-//     });
-
 //     producer.await.unwrap();
 //     consumer.await.unwrap();
 // }
-
+//     let sender_a = sender.clone();
+//     let sender_b = sender.clone();
+//     let task_a = tokio::spawn(async move {
+//         sender_a.send("Price from feed A").await.unwrap();
+//     let task_b = tokio::spawn(async move {
+//         sender_b.send("Price from feed B").await.unwrap();
+//     let first = receiver.recv().await.unwrap();
+//     let second = receiver.recv().await.unwrap();
+//     println!("{}", first);
+//     println!("{}", second);
+//     task_a.await.unwrap();
+//     task_b.await.unwrap();
 
 use tokio::sync::mpsc;
 
@@ -30,21 +37,47 @@ async fn main() {
 
     let sender_a = sender.clone();
     let sender_b = sender.clone();
+    let sender_c = sender.clone();
+
+    // We don't need the original sender anymore.
+    drop(sender);
 
     let task_a = tokio::spawn(async move {
-        sender_a.send("Price from feed A").await.unwrap();
+        for i in 1..=3 {
+            sender_a
+                .send(format!("A {}", i))
+                .await
+                .unwrap();
+        }
     });
 
     let task_b = tokio::spawn(async move {
-        sender_b.send("Price from feed B").await.unwrap();
+        for i in 1..=3 {
+            sender_b
+                .send(format!("B {}", i))
+                .await
+                .unwrap();
+        }
     });
 
-    let first = receiver.recv().await.unwrap();
-    let second = receiver.recv().await.unwrap();
+    let task_c = tokio::spawn(async move {
+        for i in 1..=3 {
+            sender_c
+                .send(format!("C {}", i))
+                .await
+                .unwrap();
+        }
+    });
 
-    println!("{}", first);
-    println!("{}", second);
+    let receiver_task = tokio::spawn(async move {
+        while let Some(value) = receiver.recv().await {
+            println!("{}", value);
+        }
+    });
 
     task_a.await.unwrap();
     task_b.await.unwrap();
+    task_c.await.unwrap();
+
+    receiver_task.await.unwrap();
 }
